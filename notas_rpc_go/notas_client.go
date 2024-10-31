d5fcbecbf4d4f6e7811e91214854feeeb46aafce
Gustavo Souza da Silva <149391594+Gustavo-silva05@users.noreply.github.com>
2024-10-31 11:32:05 -0300

    .

--- notas_rpc_go/notas_client.go
+++ notas_rpc_go/notas_client.go
@@ -1,110 +1,168 @@
-// package main
-
-// import (
-//     "fmt"
-//     "net/rpc"
-//     "os"
-// )
-
-// func main() {
-
-//     if len(os.Args) != 3 {
-//         fmt.Println("Uso:", os.Args[0], " <maquina> <nome_do_aluno>")
-//         return
-//     }
-
-//     porta := 8973
-//     maquina := os.Args[1]
-//     nome := os.Args[2]
-
-//     client, err := rpc.Dial("tcp", fmt.Sprintf("%s:%d", maquina, porta))
-//     if err != nil {
-//         fmt.Println("Erro ao conectar ao servidor:", err)
-//         return
-//     }
-//     i int := 2
-    
-//     var nota float64
-//     err = client.Call("Servidor.ObtemSaldo", nome, &nota)
-//     while (i < 5){
-//         if err != nil {
-//             fmt.Println("Erro ao obter nota:", err)
-//         } else {
-//             fmt.Printf("Nome: %s\n", nome)
-//             fmt.Printf("Saldo: %.2f\n", nota)
-//         }
-//         i := i + 1
-//     }
-// }
-
-
 package main
 
 import (
-    "fmt"
-    "net/rpc"
-    "os"
-    "bufio"
+	"bufio"
+	"fmt"
+	"net/rpc"
+	"os"
+	"strconv"
 )
 
+type Conta struct {
+	Nome  string
+	Saldo float64
+}
+
 func main() {
-    if len(os.Args) != 2 {
-        fmt.Println("Uso:", os.Args[0], "<maquina>")
-        return
-    }
+	if len(os.Args) != 2 {
+		fmt.Println("Uso:", os.Args[0], "<maquina>")
+		return
+	}
 
-    porta := 8973
-    maquina := os.Args[1]
+	porta := 8973
+	maquina := os.Args[1]
 
-    client, err := rpc.Dial("tcp", fmt.Sprintf("%s:%d", maquina, porta))
-    if err != nil {
-        fmt.Println("Erro ao conectar ao servidor:", err)
-        return
-    }
-    defer client.Close()
+	client, err := rpc.Dial("tcp", fmt.Sprintf("%s:%d", maquina, porta))
+	if err != nil {
+		fmt.Println("Erro ao conectar ao servidor:", err)
+		return
+	}
+	defer client.Close()
 
-    reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 
-    for {
-        fmt.Println("\nMenu:")
-        fmt.Println("1 - Abrir Conta")
-        fmt.Println("2 - Obter Saldo")
-        fmt.Println("3 - Sair")
-        fmt.Print("Escolha uma opção: ")
-        opcao, _ := reader.ReadString('\n')
-        opcao = opcao[:len(opcao)-1]
+	for {
+		fmt.Println("\nMenu:")
+		fmt.Println("1 - Abrir Conta")
+		fmt.Println("2 - Obter Saldo")
+		fmt.Println("3 - Fechar Conta")
+		fmt.Println("4 - Deposito")
+		fmt.Println("5 - Saque ")
+		fmt.Println("6 - Sair\n")
+		fmt.Print("Escolha uma opção: ")
+		opcao, _ := reader.ReadString('\n')
+		opcao = opcao[:len(opcao)-1]
 
-        switch opcao {
-        case "1":
-            // Chamada para AbrirConta
-            var resposta string
-            err := client.Call("Servidor.AbrirConta", struct{}{}, &resposta)
-            if err != nil {
-                fmt.Println("Erro ao abrir conta:", err)
-            } else {
-                fmt.Println("Resposta do servidor:", resposta)
-            }
+		switch opcao {
+		case "1":
+			// Chamada para AbrirConta
+			// Leitura do nome
+			fmt.Print("Digite o nome: ")
+			nome, _ := reader.ReadString('\n')
+			nome = nome[:len(nome)-1] // Remove o caractere de nova linha
 
-        case "2":
-            // Solicita o nome do cliente e obtém o saldo
-            fmt.Print("Digite o nome do cliente: ")
-            nome, _ := reader.ReadString('\n')
-            nome = nome[:len(nome)-1]
+			// Leitura do saldo
+			fmt.Print("Digite um saldo inicial: ")
+			input, _ := reader.ReadString('\n')
+			input = input[:len(input)-1] // Remove o caractere de nova linha
+			saldo, err := strconv.ParseFloat(input, 64)
+			if err != nil {
+				fmt.Println("Saldo inválido:", err)
+				continue
+			}
+			conta := Conta{
+				Nome:  nome,
+				Saldo: saldo,
+			}
 
-            var saldo float64
-            err := client.Call("Servidor.ObtemSaldo", nome, &saldo)
-            if err != nil {
-                fmt.Println("Erro ao obter saldo:", err)
-            } else {
-                fmt.Printf("Saldo de %s: %.2f\n", nome, saldo)
-            }
+			var resposta string
+			err = client.Call("Servidor.AbrirConta", conta, &resposta)
+			if err != nil {
+				fmt.Println("Erro ao abrir conta:", err)
+			} else {
+				fmt.Println("Resposta do servidor:", resposta)
+			}
 
-        case "3":
-            fmt.Println("Saindo...")
-            return
+		case "2":
+			// Solicita o nome do cliente e obtém o saldo
+			fmt.Print("Digite o nome do cliente: ")
+			nome, _ := reader.ReadString('\n')
+			nome = nome[:len(nome)-1]
 
-        default:
-            fmt.Println("Opção inválida. Tente novamente.")
-        }
-    }
+			var saldo_verify float64
+			err := client.Call("Servidor.ObtemSaldo", nome, &saldo_verify)
+			if err != nil {
+				fmt.Println("Erro ao obter saldo:", err)
+			} else {
+				fmt.Printf("Saldo de %s: %.2f\n", nome, saldo_verify)
+			}
+		case "3":
+			fmt.Print("Digite o nome: ")
+			nome, _ := reader.ReadString('\n')
+			nome = nome[:len(nome)-1] // Remove o caractere de nova linha
+			var saldo float64 = 0
+			conta := Conta{
+				Nome:  nome,
+				Saldo: saldo,
+			}
+			var resposta string
+			err = client.Call("Servidor.FecharConta", conta, &resposta)
+			if err != nil {
+				fmt.Println("Erro ao Fechar conta:", err)
+			} else {
+				fmt.Println("Resposta do servidor:", resposta)
+			}
+
+		case "4":
+			fmt.Print("Digite o nome: ")
+			nome, _ := reader.ReadString('\n')
+			nome = nome[:len(nome)-1] // Remove o caractere de nova linha
+
+			// Leitura do saldo
+			fmt.Print("Digite o deposito: ")
+			input, _ := reader.ReadString('\n')
+			input = input[:len(input)-1] // Remove o caractere de nova linha
+			saldo, err := strconv.ParseFloat(input, 64)
+			if err != nil || saldo < 0 {
+				fmt.Println("Saldo inválido:", err)
+				continue
+			}
+			conta := Conta{
+				Nome:  nome,
+				Saldo: saldo,
+			}
+
+			var resposta string
+			err = client.Call("Servidor.Deposito", conta, &resposta)
+			if err != nil {
+				fmt.Println("Erro ao abrir conta:", err)
+			} else {
+				fmt.Println("Resposta do servidor:", resposta)
+			}
+
+		case "5":
+			fmt.Print("Digite o nome: ")
+			nome, _ := reader.ReadString('\n')
+			nome = nome[:len(nome)-1] // Remove o caractere de nova linha
+
+			// Leitura do saldo
+			fmt.Print("Digite o saque: ")
+			input, _ := reader.ReadString('\n')
+			input = input[:len(input)-1] // Remove o caractere de nova linha
+			saldo, err := strconv.ParseFloat(input, 64)
+			if err != nil || saldo < 0 {
+				fmt.Println("Saldo inválido:", err)
+				continue
+			}
+			conta := Conta{
+				Nome:  nome,
+				Saldo: saldo,
+			}
+
+			var resposta string
+			err = client.Call("Servidor.Saque", conta, &resposta)
+			if err != nil {
+				fmt.Println("Erro ao abrir conta:", err)
+			} else {
+				fmt.Println("Resposta do servidor:", resposta)
+			}
+
+		case "6":
+			fmt.Println("Saindo...")
+			return
+
+		default:
+			fmt.Println("Opção inválida. Tente novamente.")
+		}
+	}
 }
--- notas_rpc_go/notas_server.go
+++ notas_rpc_go/notas_server.go
@@ -1,105 +1,118 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "net"
-    "net/rpc"
-    "os"
-    "strconv"
+	"fmt"
+	"net"
+	"net/rpc"
 )
 
 // Estrutura para representar um aluno
 type Conta struct {
-    Nome string
-    Saldo float64
+	Nome  string
+	Saldo float64
 }
 
 // Estrutura para o servidor
 type Servidor struct {
-    contas []Conta
+	contas []Conta
 }
 
 // Método para inicializar a lista de alunos no servidor
 func (s *Servidor) inicializar() {
-    s.contas = []Conta{
+	s.contas = []Conta{
 		{"Alexandre", 900.5},
-		{"Barbara",   855.5},
-		{"Joao",      656.5},
-		{"Maria",     946.0},
-		{"Paulo",    10546.0},
-		{"Pedro",     7465.0},
+		{"Barbara", 855.5},
+		{"Joao", 656.5},
+		{"Maria", 946.0},
+		{"Paulo", 10546.0},
+		{"Pedro", 7465.0},
 	}
 }
- 
+
 // Método remoto que retorna a nota de um aluno dado o seu nome
 func (s *Servidor) ObtemSaldo(nome string, saldo *float64) error {
-    for _, conta := range s.contas {
-        if conta.Nome == nome {
-            *saldo = conta.Saldo
-            return nil
-        }
-    }
-    return fmt.Errorf("Aluno %s não encontrado", nome)
+	for _, conta := range s.contas {
+		if conta.Nome == nome {
+			*saldo = conta.Saldo
+			return nil
+		}
+	}
+	return fmt.Errorf("Aluno %s não encontrado", nome)
 }
 
-// func (s *Servidor) AbrirConta() error {
-//     reader := bufio.NewReader(os.Stdin)
-// 	fmt.Print("Digite Nome: ")
-// 	input, _ := reader.ReadString('\n')
-//     Conta conta
-//     novo.Nome := input
-//     fmt.Print("Digite uma saldo inicial: ")
-// 	input, _ := reader.ReadString('\n')
-//     novo.Saldo := input
-//     s = append(s,novo)
-// }
+func (s *Servidor) AbrirConta(conta Conta, resposta *string) error {
 
-func (s *Servidor) AbrirConta() error {
-    reader := bufio.NewReader(os.Stdin)
-    // Leitura do nome
-    fmt.Print("Digite o nome: ")
-    nome, _ := reader.ReadString('\n')
-    nome = nome[:len(nome)-1] // Remove o caractere de nova linha
-    // Leitura do saldo
-    fmt.Print("Digite um saldo inicial: ")
-    input, _ := reader.ReadString('\n')
-    input = input[:len(input)-1] // Remove o caractere de nova linha
-    saldo, err := strconv.ParseFloat(input, 64)
-    if err != nil {
-        return fmt.Errorf("saldo inválido: %v", err)
-    }
-    // Criação da nova conta
-    conta := Conta{
-        Nome:  nome,
-        Saldo: saldo,
-    }
-    // Adiciona a nova conta à lista de contas
-    s.contas = append(s.contas, conta)
-    fmt.Println("Conta criada com sucesso!")
-    return nil
+	if conta.Saldo < 0 {
+		*resposta = "Erro saldo inicial invalido"
+
+	} else {
+		// Adiciona a nova conta à lista de contas
+		s.contas = append(s.contas, conta)
+		*resposta = "Conta criada com sucesso!"
+	}
+	return nil
+}
+func (s *Servidor) FecharConta(conta Conta, resposta *string) error {
+
+	for i, a := range s.contas {
+		if a.Nome == conta.Nome {
+			// Remove a conta da lista
+			*resposta = fmt.Sprintf("Conta removida com sucesso e saldo devolvido = %f", a.Saldo)
+			s.contas = append(s.contas[:i], s.contas[i+1:]...)
+			return nil
+		}
+	}
+	*resposta = "Conta não encontrada."
+	return fmt.Errorf("conta com nome %s não encontrada", conta.Nome)
 }
 
+func (s *Servidor) Deposito(conta Conta, resposta *string) error {
+
+	for _, a := range s.contas {
+		if a.Nome == conta.Nome {
+			a.Saldo += conta.Saldo
+			*resposta = fmt.Sprintf("Deposito feito, novo de %s saldo = %f", conta.Nome, a.Saldo)
+			return nil
+		}
+	}
+	*resposta = "Conta não encontrada."
+	return fmt.Errorf("conta com nome %s não encontrada", conta.Nome)
+}
+
+func (s *Servidor) Saque(conta Conta, resposta *string) error {
+
+	for _, a := range s.contas {
+		if a.Nome == conta.Nome {
+			a.Saldo -= conta.Saldo
+			*resposta = fmt.Sprintf("Saque feito, novo de %s saldo = %f", conta.Nome, a.Saldo)
+			return nil
+		}
+	}
+	*resposta = "Conta não encontrada."
+	return fmt.Errorf("conta com nome %s não encontrada", conta.Nome)
+}
 
 func main() {
-    porta := 8973
-    servidor := new(Servidor)
-    servidor.inicializar()
+	porta := 8973
+	servidor := new(Servidor)
+	servidor.inicializar()
+	// var resposta string
+	// servidor.AbrirConta(struct{}{}, &resposta)
 
-    rpc.Register(servidor)
-    l, err := net.Listen("tcp", fmt.Sprintf(":%d", porta))
-    if err != nil {
-        fmt.Println("Erro ao iniciar o servidor:", err)
-        return
-    }
+	rpc.Register(servidor)
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", porta))
+	if err != nil {
+		fmt.Println("Erro ao iniciar o servidor:", err)
+		return
+	}
 
-    fmt.Println("Servidor aguardando conexões na porta", porta)
-    for {
-        conn, err := l.Accept()
-        if err != nil {
-            fmt.Println("Erro ao aceitar conexão:", err)
-            continue
-        }
-        go rpc.ServeConn(conn)
-    }
+	fmt.Println("Servidor aguardando conexões na porta", porta)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Erro ao aceitar conexão:", err)
+			continue
+		}
+		go rpc.ServeConn(conn)
+	}
 }
