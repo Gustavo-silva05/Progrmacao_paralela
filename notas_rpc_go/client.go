f42297499980b177ea0746bd96b9d6deac75a4a2
Gustavo Souza <gus.ssilva05@gmail.com>
2024-11-03 15:06:12 -0300

    .

---
+++ notas_rpc_go/client.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"net/rpc"
+	"os"
+	"sync"
+)
+
+var contas_novas = []string{"Bruno", "Sofia", "Izis"}
+var contas_antigas = []string{"Maria", "Pedro", "Joao"}
+
+var wg sync.WaitGroup
+
+type Conta struct {
+	Nome  string
+	Saldo float64
+}
+
+func SALDO(nome string, porta int, maquina string, cliente *rpc.Client) {
+	var resposta string
+	var err = cliente.Call("Servidor.ObtemSaldo", nome, &resposta)
+	if err != nil {
+		fmt.Println("Erro ao ver saldo conta:", err)
+	} else {
+		fmt.Println("Resposta do servidor:", resposta)
+	}
+
+}
+
+func ABRIR(nome string, porta int, maquina string, cliente *rpc.Client) {
+	var resposta string
+	var err = cliente.Call("Servidor.AbrirConta", nome, &resposta)
+	if err != nil {
+		fmt.Println("Erro ao abrir conta:", err)
+	} else {
+		fmt.Println("Resposta do servidor:", resposta)
+	}
+
+}
+func FECHAR(nome string, porta int, maquina string, client *rpc.Client) {
+	var resposta string
+	var err = client.Call("Servidor.FecharConta", nome, &resposta)
+	if err != nil {
+		fmt.Println("Erro ao fechar conta:", err)
+	} else {
+		fmt.Println("Resposta do servidor:", resposta)
+	}
+
+}
+
+func DEPOSITO(nome string, porta int, maquina string, client *rpc.Client) {
+	var resposta string
+	var err = client.Call("Servidor.Deposito", Conta{Nome: nome, Saldo: 200}, &resposta)
+	if err != nil {
+		fmt.Println("Erro no deposito conta:", err)
+	} else {
+		fmt.Println("Resposta do servidor:", resposta)
+	}
+	wg.Done()
+
+}
+func SAQUE(nome string, porta int, maquina string, client *rpc.Client) {
+	var resposta string
+	var err = client.Call("Servidor.Saque", Conta{Nome: nome, Saldo: 100}, &resposta)
+	if err != nil {
+		fmt.Println("Erro no saque conta:", err)
+	} else {
+		fmt.Println("Resposta do servidor:", resposta)
+	}
+	wg.Done()
+
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Uso:", os.Args[0], "<maquina>")
+		return
+	}
+	porta := 8973
+	maquina := os.Args[1]
+	client, err := rpc.Dial("tcp", fmt.Sprintf("%s:%d", maquina, porta))
+	if err != nil {
+		fmt.Println("Erro ao conectar ao servidor:", err)
+		return
+	}
+	defer client.Close()
+
+	for i := 0; i < 3; i++ {
+		ABRIR(contas_novas[i], porta, maquina, client)
+	}
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go DEPOSITO(contas_novas[i], porta, maquina, client)
+	}
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go SAQUE(contas_antigas[i], porta, maquina, client)
+	}
+	wg.Wait()
+	// for i := 0; i < 3; i++ {
+	// 	FECHAR(contas_antigas[i], porta, maquina, client)
+	// }
+
+}
---
+++ notas_rpc_go/server.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/rpc"
+	"sync"
+)
+
+var mutex sync.Mutex
+
+// Estrutura para representar um aluno
+type Conta struct {
+	Nome  string
+	Saldo float64
+}
+
+// Estrutura para o servidor
+type Servidor struct {
+	contas []Conta
+}
+
+// Método para inicializar a lista de alunos no servidor
+func (s *Servidor) inicializar() {
+	s.contas = []Conta{
+		{"Alexandre", 900.5},
+		{"Barbara", 855.5},
+		{"Joao", 656.5},
+		{"Maria", 946.0},
+		{"Paulo", 10546.0},
+		{"Pedro", 7465.0},
+	}
+}
+
+// Método remoto que retorna o saldo de um cliente dado o seu nome
+func (s *Servidor) ObtemSaldo(nome string, resposta *string) error {
+	for _, conta := range s.contas {
+		if conta.Nome == nome {
+			fmt.Println("Saldo verificado para ", nome)
+			*resposta = fmt.Sprintf("Conta de %s com R$ %g", nome, conta.Saldo)
+			return nil
+		}
+	}
+	return fmt.Errorf("Aluno %s não encontrado", nome)
+}
+
+func (s *Servidor) AbrirConta(nome string, resposta *string) error {
+	conta := Conta{
+		Nome:  nome,
+		Saldo: 0.0,
+	}
+	s.contas = append(s.contas, conta)
+	fmt.Println("Conta criada para ", nome)
+	*resposta = fmt.Sprintf("Conta de %s criada com sucesso!", nome)
+	return nil
+}
+
+func (s *Servidor) FecharConta(nome string, resposta *string) error {
+
+	for i, a := range s.contas {
+		if a.Nome == nome {
+			mutex.Lock()
+			// Remove a conta da lista
+			fmt.Println("Conta excluida de ", nome)
+			*resposta = fmt.Sprintf("Conta removida com sucesso e saldo devolvido = %g", a.Saldo)
+			s.contas = append(s.contas[:i], s.contas[i+1:]...)
+			mutex.Unlock()
+			return nil
+
+		}
+	}
+	*resposta = "Conta não encontrada."
+	return fmt.Errorf("conta com nome %s não encontrada", nome)
+}
+
+func (s *Servidor) Deposito(conta Conta, resposta *string) error {
+
+	for i, a := range s.contas {
+		if a.Nome == conta.Nome {
+			mutex.Lock()
+			s.contas[i].Saldo += conta.Saldo
+			fmt.Println("Desposito Realizado com sucesso ")
+			*resposta = fmt.Sprintf("Deposito de %g feito, novo saldo de %s = %g", conta.Saldo, conta.Nome, s.contas[i].Saldo)
+			mutex.Unlock()
+			return nil
+		}
+	}
+	*resposta = "Conta não encontrada."
+	return fmt.Errorf("conta com nome %s não encontrada", conta.Nome)
+}
+
+func (s *Servidor) Saque(conta Conta, resposta *string) error {
+
+	for i, a := range s.contas {
+		if a.Nome == conta.Nome {
+			mutex.Lock()
+			s.contas[i].Saldo -= conta.Saldo
+			fmt.Println("Saque Realizado com sucesso ")
+			*resposta = fmt.Sprintf("Saque de %g feito, novo saldo de %s = %g", conta.Saldo, conta.Nome, s.contas[i].Saldo)
+			mutex.Unlock()
+			return nil
+		}
+	}
+	*resposta = "Conta não encontrada."
+	return fmt.Errorf("conta com nome %s não encontrada", conta.Nome)
+}
+
+func main() {
+	porta := 8973
+	servidor := new(Servidor)
+	servidor.inicializar()
+	rpc.Register(servidor)
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", porta))
+	if err != nil {
+		fmt.Println("Erro ao iniciar o servidor:", err)
+		return
+	}
+
+	fmt.Println("Servidor aguardando conexões na porta", porta)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Erro ao aceitar conexão:", err)
+			continue
+		}
+		go rpc.ServeConn(conn)
+	}
+}
